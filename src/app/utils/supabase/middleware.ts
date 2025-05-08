59ba16c44678d612adca5e4abe9e2e3d61cbd0d3
justindal <justindaludado@cmail.carleton.ca>
2025-05-08 16:09:04 -0400

    update supabase middleware

--- src/app/utils/supabase/middleware.ts
+++ src/app/utils/supabase/middleware.ts
@@ -1,4 +1,4 @@
-import { createServerClient } from '@supabase/ssr'
+import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function updateSession(request: NextRequest) {
@@ -14,7 +14,7 @@ export async function updateSession(request: NextRequest) {
         get(name: string) {
           return request.cookies.get(name)?.value
         },
-        set(name: string, value: string, options: any) {
+        set(name: string, value: string, options: CookieOptions) {
           request.cookies.set({
             name,
             value,
@@ -29,7 +29,7 @@ export async function updateSession(request: NextRequest) {
             ...options,
           })
         },
-        remove(name: string, options: any) {
+        remove(name: string, options: CookieOptions) {
           request.cookies.delete(name)
 
           supabaseResponse = NextResponse.next({
